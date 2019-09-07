e273260dd770ef248efa75d0f48d072937a957fa
Ang <zegestudio@gmail.com>
2019-09-07 15:21:16 +0800

    添加git与github链接

---
+++ 1.编程准备/1.2Git/6.git库与github库连接.go
@@ -0,0 +1,13 @@
+//在github中创建好库后，会显示入如下代码：
+git remote add origin git@github.com:Zegele/GoNote.git
+git push -u origin master
+
+
+
+
+git remote add origin git@github.com:Zegele/GoNote.git
+//将这段代码粘贴进git中（此时的git是项目的位置），这表示本地代码库和github中名叫GoNote的库联系在一起了。
+
+//然后输入以下代码：
+git push -u origin master
+//等待上传，git中显示上传进度，上传完毕后，刷新github，则可以在github中刚看到相应的代码文件。
