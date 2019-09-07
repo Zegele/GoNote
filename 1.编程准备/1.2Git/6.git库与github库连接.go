05614284860c2580b9d92daab325f598f097d402
Ang <zegestudio@gmail.com>
2019-09-07 15:25:02 +0800

    添加git与github链接

--- 1.编程准备/1.2Git/6.git库与github库连接.go
+++ 1.编程准备/1.2Git/6.git库与github库连接.go
@@ -11,3 +11,6 @@ git remote add origin git@github.com:Zegele/GoNote.git
 //然后输入以下代码：
 git push -u origin master
 //等待上传，git中显示上传进度，上传完毕后，刷新github，则可以在github中刚看到相应的代码文件。
+
+//以后更新代码后，git add .  --> git commit -m'xxx' --> git push即可上传到github
+//以后的git push后可以不用跟分支，默认push当前支。
