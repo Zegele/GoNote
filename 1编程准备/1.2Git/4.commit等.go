69be9f0c372e43e572f4e2ef487fc65d5043eac3
Ang <623045191@qq.com>
2021-06-06 13:09:25 +0800

    补充

--- 1编程准备/1.2Git/4.commit等.go
+++ 1编程准备/1.2Git/4.commit等.go
@@ -5,3 +5,12 @@ git add name//表示添加name这个文件。
 //git status查看被提交文件的状态。
 2.
 git commit -m'描述'//注意：描述是必须写的，描述这次提交的标志。
+
+连接到github可参考：
+echo "# xuexiqianfeng" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Zegele/xuexiqianfeng.git
+git push -u origin main
